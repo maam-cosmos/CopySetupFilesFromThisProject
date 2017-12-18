6c736a1153415b489ee6633bd74c699d6e970bb3
metafour <abdullah.almomin@metafour.com>
2017-12-18 12:05:18 +0600

    controller code remove

--- src/com/copysetupfiles/HomeController.java
+++ src/com/copysetupfiles/HomeController.java
@@ -10,20 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 	
-	/*@RequestMapping(value="/")
+	@RequestMapping(value="/")
 	public String returnIndexPage (){
 		return "index";
-	}*/
-	
-	@GetMapping("/")
-    public String greetingForm(Model model) {
-        model.addAttribute("checkboxvalues", new CheckBoxValues());
-        return "index";
-    }
-
-    @PostMapping("/")
-    public String greetingSubmit(@ModelAttribute CheckBoxValues checkboxvalues) {
-        return "index";
-    }
+	}
 
 }
