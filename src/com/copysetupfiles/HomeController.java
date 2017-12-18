f1a9e36b88f26091ddf93134c13956c415dbb1a8
metafour <abdullah.almomin@metafour.com>
2017-12-18 12:07:01 +0600

    imported class remove

--- src/com/copysetupfiles/HomeController.java
+++ src/com/copysetupfiles/HomeController.java
@@ -1,10 +1,6 @@
 package com.copysetupfiles;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
